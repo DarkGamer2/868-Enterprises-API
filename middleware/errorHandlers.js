ac1b81483cf11e04886423106d5d2f82384c3a8c
Kameer Hosein <kameer-hosien@outlook.com>
2024-10-07 21:03:31 -0400

    converted errorHandlers to work with common modules

--- middleware/errorHandlers.js
+++ middleware/errorHandlers.js
@@ -1,15 +1,20 @@
 // Not found handler
-export const notFoundHandler = (req, res, next) => {
+const notFoundHandler = (req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`);
   res.status(404);
   next(error);
 };
 
 // General error handler
-export const errorHandler = (err, req, res, next) => {
+const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode).json({
     message: err.message,
     stack: process.env.NODE_ENV === 'production' ? null : err.stack,
   });
+};
+
+module.exports = {
+  notFoundHandler,
+  errorHandler,
 };
